fbfddcf08dff062d4bc67d5908b4f350e3ab662d
Jessica Watts <jessperr182@hotmail.com>
2020-09-11 05:58:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator( attendees)
+    attendees = []
+    attendees.each do |people| puts "Hello, my name is #{people}"
+    array << badge_maker(name)
+    end
+end
+
+def assign_rooms (attendees)
+  counter = 1 
+  room_assignment=[]
+  attendees.each do |people|
+    room _assignment<< "Hello #{people}! You'll be assigned to room #{counter}!"
+    counter +=1 
+  end
+  return room_assignment
+end
+
+def printer(attendees)
+  attendees.each do |stuff|
+    puts "#{stuff}"
+  end
+end
+  
+
+
+  
